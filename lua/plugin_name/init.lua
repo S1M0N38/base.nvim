a18fd6a4792f146327c23425bfdbce3cbe075dbd
S1M0N38 <bertolottosimone@gmail.com>
2024-01-11 17:47:17 +0100

    Fix linting issue

--- lua/plugin_name/init.lua
+++ lua/plugin_name/init.lua
@@ -2,7 +2,8 @@
 
 ---@brief [[
 ---This is a template for a plugin. It is meant to be copied and modified.
----The following code is a simple example to show how to use this template and how to take advantage of code documentation to generate plugin documentation.
+---The following code is a simple example to show how to use this template and how to take advantage of code
+---documentation to generate plugin documentation.
 ---
 ---This simple example plugin provides a command to calculate the maximum or minimum of two numbers.
 ---Moreover, the result can be rounded if specified by the user in its configuration using the setup function.
