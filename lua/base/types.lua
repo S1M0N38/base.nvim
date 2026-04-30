4ab73ca2c933c94ac0f6f421014c271e4c9f216b
S1M0N38 <bertolottosimone@gmail.com>
2026-04-30 18:35:30 +0200

    refactor(types): update type annotations for new API
    - Add did_setup, augroup, ns fields
    - Use function signature syntax in @field annotations
    - Add Base.Util class with notify/info/warn/error methods

--- lua/base/types.lua
+++ lua/base/types.lua
@@ -5,16 +5,17 @@
 -- lua/base/init.lua -----------------------------------------------------------
 
 ---@class Base.Plugin
----@field setup function setup the plugin with user options
----@field hello function Say hello to the user using configured name
----@field bye function Say goodbye to the user using configured name
+---@field did_setup boolean whether setup() has been called
+---@field setup fun(opts?: Base.UserOptions) setup the plugin with user options
+---@field hello fun(): string Say hello to the user using configured name
+---@field bye fun(): string Say goodbye to the user using configured name
 
 -- lua/base/config.lua ---------------------------------------------------------
 
 ---@class Base.Config
----@field defaults Base.DefaultOptions default plugin options
----@field options Base.Options merged user and default options
----@field setup function setup the plugin configuration
+---@field augroup integer augroup created at module load
+---@field ns integer namespace created at module load
+---@field setup fun(opts?: Base.UserOptions) setup the plugin configuration
 
 ---@class Base.UserOptions
 ---@field name? string The name of the user to greet (optional)
@@ -25,7 +26,15 @@
 ---@class Base.Options
 ---@field name string The name of the user to greet (merged from user/default options)
 
+-- lua/base/util.lua -----------------------------------------------------------
+
+---@class Base.Util
+---@field notify fun(msg: string|table, level?: integer) send notification with plugin title
+---@field info fun(msg: string) send info notification
+---@field warn fun(msg: string) send warning notification
+---@field error fun(msg: string) send error notification
+
 -- lua/base/health.lua ---------------------------------------------------------
 
 ---@class Base.Health
----@field check fun(): nil perform health check for the plugin
+---@field check fun() perform health check for the plugin
